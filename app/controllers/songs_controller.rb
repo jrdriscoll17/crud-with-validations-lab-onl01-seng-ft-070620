85299884e67940fa381ed202fb2e4ec7c6c76467
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-09-12 03:44:36 -0500

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def update
+    @song = Song.find(params[:id])
+
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+
+    redirect_to songs_path if @song.destroy
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
+  end
+end
